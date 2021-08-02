cc7b4196bf1525982b8d1f0601344991e8b92ff6
Karan Singh Solanki <87369849+Metacube-KaranSingh-Solanki@users.noreply.github.com>
2021-08-02 17:50:56 +0530

    Minor updation

--- Session 3 Trees and Graphs/Cricket Bowling Problem/src/BowlerScheduler.java
+++ Session 3 Trees and Graphs/Cricket Bowling Problem/src/BowlerScheduler.java
@@ -1,42 +1,52 @@
 import java.util.ArrayList;
 import java.util.List;
+
 public class BowlerScheduler {
-	
+
 	Heap heap;
-	
+	private final int ballsToPlay;
+
 	/**
 	 * Constructor to initialize the heap
+	 * 
 	 * @param bowlers
 	 */
-	public BowlerScheduler(Bowler[] bowlers) {
-		 heap = new Heap();
-		 if (bowlers.length == 0)
-			 throw new AssertionError("Bowlers are missing.");
-		 for (Bowler bowler: bowlers) {
-			 heap.insert(bowler);
-		 }
+	public BowlerScheduler(Bowler[] bowlers, int ballsToPlay) {
+		if (ballsToPlay < 0)
+			throw new AssertionError("Enter valid balls to play");
+		this.ballsToPlay = ballsToPlay;
+		heap = new Heap();
+		if (bowlers.length == 0)
+			throw new AssertionError("Bowlers are missing.");
+		for (Bowler bowler : bowlers) {
+			heap.insert(bowler);
+		}
 	}
-	
+
 	/**
 	 * Returns the bowling order as an array of bowler names
+	 * 
 	 * @return array of bowler names with correct order
 	 */
 	public String[] getBowlingOrder() {
 		List<String> order = new ArrayList<>();
-		while (!heap.isEmpty()) {
+		int count = ballsToPlay;
+		while (!heap.isEmpty() && count != 0) {
 			Bowler bowler = heap.extractMax();
 			order.add(bowler.getName());
 			bowler.setBallsLeft(bowler.getBallsLeft() - 1);
 			if (bowler.getBallsLeft() != 0)
 				heap.insert(bowler);
+			count--;
 		}
 		Object[] str = order.toArray();
 		String[] orderArray = new String[str.length];
 
-		for (int i=0;i<str.length;i++){
+		for (int i = 0; i < str.length; i++) {
 			orderArray[i] = (String) str[i];
 		}
 
 		return orderArray;
 	}
+
 }
--- Session 3 Trees and Graphs/Cricket Bowling Problem/src/BowlerSchedulerTest.java
+++ Session 3 Trees and Graphs/Cricket Bowling Problem/src/BowlerSchedulerTest.java
@@ -10,30 +10,54 @@ public class BowlerSchedulerTest {
 	@Test
 	public void TestAssertionErrorIfBowlersAreZero() {
 		Assertions.assertThrows(AssertionError.class, () -> {
-			new BowlerScheduler(new Bowler[] {});
+			new BowlerScheduler(new Bowler[] {}, 15);
 		});
+	}
+
+	@Test
+	public void TestOneBallPlayed() {
+		Bowler[] bowlers = new Bowler[] { new Bowler("B1", 2), new Bowler("B2", 2), new Bowler("B3", 4) };
+
+		bowlerScheduler = new BowlerScheduler(bowlers, 1);
+		String[] expectedOutput = new String[] { "B3" };
+
+		assertArrayEquals(expectedOutput, bowlerScheduler.getBowlingOrder());
 	}
 
 	@Test
 	public void TestOnValidInputMultipleBowlers() {
 		Bowler[] bowlers = new Bowler[] { new Bowler("B1", 2), new Bowler("B2", 2), new Bowler("B3", 4),
 				new Bowler("B4", 1), new Bowler("B5", 3), new Bowler("B6", 3) };
-		
-		bowlerScheduler = new BowlerScheduler(bowlers);
-		String[] expectedOutput = new String[] {
-				"B3", "B5", "B3", "B6", "B3", "B6", "B1", "B2","B5","B2","B5","B3","B4","B1","B6"
-		};
-		
+
+		bowlerScheduler = new BowlerScheduler(bowlers, 15);
+		String[] expectedOutput = new String[] { "B3", "B5", "B3", "B6", "B3", "B6", "B1", "B2", "B5", "B2", "B5", "B3",
+				"B4", "B1", "B6" };
+
 		assertArrayEquals(expectedOutput, bowlerScheduler.getBowlingOrder());
 	}
-	
+
 	@Test
 	public void TestOnOneBowler() {
-		String[] expectedOutput = new String[] {
-				"B1", "B1", "B1", "B1"
-		};
-		
-		bowlerScheduler = new BowlerScheduler(new Bowler[] {new Bowler("B1", 4)});
+		String[] expectedOutput = new String[] { "B1", "B1", "B1", "B1" };
+
+		bowlerScheduler = new BowlerScheduler(new Bowler[] { new Bowler("B1", 4) }, 4);
 		assertArrayEquals(expectedOutput, bowlerScheduler.getBowlingOrder());
 	}
+
+	@Test
+	public void TestWhenBallsPlayZero() {
+		Bowler[] bowlers = new Bowler[] { new Bowler("B1", 2), new Bowler("B2", 3) };
+		String[] expectedOutput = new String[] {};
+
+		bowlerScheduler = new BowlerScheduler(bowlers, 0);
+		assertArrayEquals(expectedOutput, bowlerScheduler.getBowlingOrder());
+	}
+
+	@Test
+	public void TestAssertionErrorIfBallsPlayedNegative() {
+		Assertions.assertThrows(AssertionError.class, () -> {
+			new BowlerScheduler(new Bowler[] {}, -1);
+		});
+	}
+
 }
