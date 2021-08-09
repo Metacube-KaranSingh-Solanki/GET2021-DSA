6e14f9fc35dd70bfa9224e1b5ab4d151863b6a37
Karan Singh Solanki <87369849+Metacube-KaranSingh-Solanki@users.noreply.github.com>
2021-08-09 18:20:17 +0530

    Corrected minor typo

--- Algorithmic Analysis Session 2/Graph Dijkstra Algorithm/src/Edge.java
+++ Algorithmic Analysis Session 2/Graph Dijkstra Algorithm/src/Edge.java
@@ -1,51 +1,50 @@
 
 /**
- * Represents a single edge with source, destination and weight
+ * Represent a single edge
  */
-public class Edge implements Comparable<Edge>{
-	
-		int source;
-		int destination;
-		int weight;
-		
-		public Edge(int source, int destination, int weight) {
-			this.source = source;
-			this.destination = destination;
-			this.weight = weight;
-		}
+public class Edge implements Comparable<Edge> {
 
-		@Override
-		public String toString() {
-			return "WeightedEdge [source=" + source + ", destination=" + destination + ", weight=" + weight + "]";
-		}
-		
-		@Override
-		public int hashCode() {
-			return source+destination;
-		}
+	int source;
+	int destination;
+	int weight;
 
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Edge other = (Edge) obj;
-			return ((source==other.source && destination==other.destination)||(source==other.destination && destination==other.source));
-		}
+	public Edge(int source, int destination, int weight) {
+		this.source = source;
+		this.destination = destination;
+		this.weight = weight;
+	}
 
-		@Override
-		public int compareTo(Edge o) {
-			if(weight>o.weight)
-				return 1;
-			else if(weight == o.weight){
-				return 0;
-			}
-			return -1;
-		}
+	@Override
+	public String toString() {
+		return "WeightedEdge [source=" + source + ", destination=" + destination + ", weight=" + weight + "]";
+	}
 
-		
-		
+	@Override
+	public int hashCode() {
+		return source + destination;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Edge other = (Edge) obj;
+		return ((source == other.source && destination == other.destination)
+				|| (source == other.destination && destination == other.source));
+	}
+
+	@Override
+	public int compareTo(Edge o) {
+		if (weight > o.weight)
+			return 1;
+		else if (weight == o.weight) {
+			return 0;
+		}
+		return -1;
+	}
+
 }
